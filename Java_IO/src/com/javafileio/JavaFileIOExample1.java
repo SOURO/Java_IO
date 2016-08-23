7e0414623defb03c326ab4a673d63f1a204aa737
geekyStart <soura1990@gmail.com>
2016-08-23 17:05:33 +0530

    Changing for demo

--- Java_IO/src/com/javafileio/JavaFileIOExample1.java
+++ Java_IO/src/com/javafileio/JavaFileIOExample1.java
@@ -6,7 +6,7 @@ public class JavaFileIOExample1 {
 	public static void main(String args[]) {
 		try {
 			FileOutputStream fout = new FileOutputStream("demo.txt");
-			String s = "Demo for IntelliJ Git Integration";
+			String s = "Doing changes for branch: temp_branch";
 			byte b[] = s.getBytes();
 			fout.write(b);
 			fout.close();
