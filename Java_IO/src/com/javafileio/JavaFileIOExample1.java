26c804f8b9111d317a4ee10cc6dac184cb3c50fb
geekyStart <soura1990@gmail.com>
2016-08-23 17:55:35 +0530

    Changing for demo

--- Java_IO/src/com/javafileio/JavaFileIOExample1.java
+++ Java_IO/src/com/javafileio/JavaFileIOExample1.java
@@ -5,8 +5,10 @@ import java.io.FileOutputStream;
 public class JavaFileIOExample1 {
 	public static void main(String args[]) {
 		try {
+			/* Creating FileOutputStream object here to write in a file*/
 			FileOutputStream fout = new FileOutputStream("demo.txt");
 			String s = "Doing changes for branch: temp_branch";
+			/*Converting string to byte and then storing it into byte array */
 			byte b[] = s.getBytes();
 			fout.write(b);
 			fout.close();
